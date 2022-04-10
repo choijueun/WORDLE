8c848a4096dcbf5a3859e5d24c339140fa42446f
choijueun <songjueun815@gmail.com>
2022-04-10 23:24:25 +0900

    JS function 정리

--- view/wordle.js
+++ view/wordle.js
@@ -1,51 +1,19 @@
 $(function(){
     // DRAG UNABLE
-    // document.oncontextmenu = function(){ return false; }
     $(document).on('contextmenu dragstart selectstart keydown', function(e){ return false; })
 
-    // REGULAR EXPRESSION
-    function kRe(str) {
-        // 한글 정규식
-        const re = /[ㄱ-ㅎㅏ-ㅣ가-힣]/g; 
-        // 검사
-        if (re.test(str)) { return true; }
-        else { return false; }
-    }
-
     // SHIFT
-    function toggleShift(){
-        // SHIFT 영역
-        let vr_shift = $('.vr-shift')
-        // SHIFT 토글
-        vr_shift.toggleClass('disabled').toggleClass('inline-block')
-    }
-    $(document).on( 'click', '#SHIFT', function(){ toggleShift(); } );
+    $(document).on( 'click', '#SHIFT', function(){ 
+        toggleShift(); 
+    });
 
     // DELETE
     $(document).on( 'click', '#DELETE', function(){
         console.log('click #DELETE')
     });
-
+    
     // SUBMIT
     $(document).on( 'click', '#SUBMIT', function(){
         console.log('click #SUBMIT')
-    });
-
-    // AJAX
-    var answer = $('#answer').val()
-    $.ajax({
-        url: "/k4rdle",
-        method: "POST",
-        data: {
-            answer: answer
-        },
-        dataType : "json",
-        contentType:"application/json"
-    })
-    .done(function (msg) {
-        alert(msg);
-    })
-    .fail(function (jqXHR, textStatus) {
-        alert("Request failed: " + textStatus);
     });
 });
