06f57d12fcbef07afbc49c9c3d38193ea87355d1
CJE <songjueun815@gmail.com>
2022-04-22 17:41:07 +0900

    header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-
+            <header className="text-center">
+                <span className="word-success">K</span><span className="word-warning">4</span>RDLE
+                <nav className="text-center">
+                    how to play
+                </nav>
+            </header>
         </div>
     );
 }
