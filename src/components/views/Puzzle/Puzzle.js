337023747f87d98dcb44c3f74728e37d69324585
choijueun <songjueun815@gmail.com>
2022-05-06 11:00:13 +0900

    PuzzleBlock text x

--- src/components/views/Puzzle/Puzzle.js
+++ src/components/views/Puzzle/Puzzle.js
@@ -4,10 +4,10 @@ import './Puzzle.css'
 function Puzzle() {
     return (
         <div class="row">
-            <div className="Puzzle block-word text-center round">사</div>
-            <div className="Puzzle block-word text-center round">자</div>
-            <div className="Puzzle block-word text-center round">성</div>
-            <div className="Puzzle block-word text-center round">어</div>
+            <div className="Puzzle block-word text-center round"></div>
+            <div className="Puzzle block-word text-center round"></div>
+            <div className="Puzzle block-word text-center round"></div>
+            <div className="Puzzle block-word text-center round"></div>
         </div>
     )
 }
