9889dc983cdf2827220e644e8ce117bed2bb301c
choijueun <songjueun815@gmail.com>
2022-07-21 15:10:09 +0900

    주석

--- wordle-eng/src/App.js
+++ wordle-eng/src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      {/* 제목 */}
+
+      {/* 네모네모 */}
+
+      {/* 키보드 */}
+
+      {/* Modal */}
     </div>
   );
 }
